f4dabdad4aa112c6456178f519c824dd447fdbe2
Colton <colton.randall@outlook.com>
2021-05-13 11:50:09 +1200

    Added a button to show or hide about me content

---
+++ website.js
@@ -0,0 +1,12 @@
+
+function showAboutme() {
+
+    const button = document.getElementById("ABOUTME");
+    if (button.style.display === "none"){
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    
+    }
+
+}

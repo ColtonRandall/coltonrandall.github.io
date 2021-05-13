ef444dfd874d5573235f13b5ed5642ec082947e3
Colton <colton.randall@outlook.com>
2021-05-13 12:00:24 +1200

    implemented an auto-scroll function to button

--- website.js
+++ website.js
@@ -1,10 +1,15 @@
 
 function showAboutme() {
 
+    // get the div to manipulate
     const button = document.getElementById("ABOUTME");
+    // if the div is not visible, make it visible on click
     if (button.style.display === "none"){
         button.style.display = "block";
+        // smoothly scroll down to the about me section when the button is clicked
+        document.getElementById("ABOUTME").scrollIntoView({behavior: "smooth"});
     } else {
+        // hide the div if the button is clicked again
         button.style.display = "none";
     
     }
